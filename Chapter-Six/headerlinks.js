c4b26c3e3468e9a39bcf006c67d5684cd672bb9a
Sufyan bin Uzayr <42644879+sufyanism@users.noreply.github.com>
2018-09-05 20:20:15 +0530

    Add files via upload

---
+++ Chapter-Six/headerlinks.js
@@ -0,0 +1,16 @@
+// Display all header links 
+
+module.exports = {
+  themeConfig: {
+    displayAllHeaders: true // Default is false
+  }
+ }
+
+
+// Do not highlight active header links
+
+module.exports = {
+  themeConfig: {
+    activeHeaderLinks: false, // Default is true
+  }
+ }
