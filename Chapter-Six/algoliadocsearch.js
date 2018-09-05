2be9133ccc202740dc1abb796763de5130c66e91
Sufyan bin Uzayr <42644879+sufyanism@users.noreply.github.com>
2018-09-05 20:21:48 +0530

    Add files via upload
    code to add Algolia search (must have API)

---
+++ Chapter-Six/algoliadocsearch.js
@@ -0,0 +1,11 @@
+// Add this to config.js only.
+//Be sure to provide your Algolia API and Index. 
+
+module.exports = {
+  themeConfig: {
+    algolia: {
+      apiKey: '<API>',
+      indexName: '<INDEX>'
+    }
+  }
+ } 
